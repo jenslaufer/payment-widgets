7d818421292dc8e2ce6ad8c940fbfcb33ef96db0
Jens Laufer <jens.laufer@solytics.de>
2026-02-21 14:17:47 +0100

    added extpay

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css'
 
 export { default as PaymentDetails } from './widgets/PaymentDetails.vue'
+export { default as ExtPay } from './widgets/ExtPay.vue'
 export { default as Stripe } from './widgets/Stripe.vue'
---
+++ src/widgets/ExtPay.vue
@@ -0,0 +1,27 @@
+<template>
+    <slot name="paid" v-if="hasPaid" />
+    <slot name="unpaid" v-else />
+</template>
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+    extensionId: {
+        type: Number,
+        required: true
+    }
+})
+
+import ExtPay from 'extpay'
+
+const extpay = ExtPay(props.extensionId)
+const hasPaid = ref(false)
+
+onMounted(async () => {
+    const user = await extpay.getUser().catch(() => null)
+    if (user && user.paidAt) {
+        hasPaid.value = true
+    }
+})
+
+</script>
