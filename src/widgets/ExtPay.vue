8571e63267894768120358c2930805a5a92dbc25
Jens Laufer <jens.laufer@solytics.de>
2026-02-24 15:36:07 +0100

    added  payment function

--- src/widgets/ExtPay.vue
+++ src/widgets/ExtPay.vue
@@ -17,11 +17,12 @@ import ExtPay from 'extpay'
 const extpay = ExtPay(props.extensionId)
 const hasPaid = ref(false)
 
+const openPaymentPage = () => extpay.openPaymentPage()
+
 onMounted(async () => {
     const user = await extpay.getUser().catch(() => null)
     if (user && user.paidAt) {
         hasPaid.value = true
     }
 })
-
 </script>
