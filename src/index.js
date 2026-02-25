cd5f53c69521cef247ef43e7d8a9f01c55bfdbb2
Jens Laufer <jens.laufer@solytics.de>
2026-02-25 10:40:12 +0100

    moved ext-pay widget to own lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './style.css'
 
-export { default as PaymentWidgets } from "./plugin"
 export { default as PaymentDetails } from './widgets/PaymentDetails.vue'
-export { default as ExtPay } from './widgets/ExtPay.vue'
 export { default as Stripe } from './widgets/Stripe.vue'
